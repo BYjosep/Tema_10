7167eae6579cb93f55912454a6648d4539769f23
Josep Vicent Mestre Llobell <59211313+BYjosep@users.noreply.github.com>
2025-05-06 10:24:16 +0200

    feat: Electrodoméstico.java: Refactorización de constructor y métodos

--- src/main/java/com/BYjosep04/ejercicio4/Electrodomestico.java
+++ src/main/java/com/BYjosep04/ejercicio4/Electrodomestico.java
@@ -1,6 +1,7 @@
 package com.BYjosep04.ejercicio4;
 
 
+import java.util.Objects;
 
 public class Electrodomestico {
     private final double precioBase;
@@ -8,19 +9,19 @@ public class Electrodomestico {
     private final Consumo consumoEnergetico;
     private final double peso;
 
-    public Electrodomestico(double precioBase, Colores color,
-                            Consumo consumoEnergetico, double peso) {
+    public Electrodomestico(double precioBase, String color,
+                            char consumo, double peso) {
         this.precioBase = precioBase;
-        this.color = color;
-        this.consumoEnergetico = consumoEnergetico;
+        this.color = comprobarColor(color);
+        this.consumoEnergetico = comprobarConsumoEnergetico(consumo);
         this.peso = peso;
     }
 
     public Electrodomestico() {
-        this(100.0, Colores.BLANCO, Consumo.F, 5);
+        this(100.0, " ", 'f', 5);
     }
     public Electrodomestico(double precioBase, double peso) {
-        this(precioBase, Colores.BLANCO, Consumo.F, peso);
+        this(precioBase," ", 'f', peso);
     }
 
 
@@ -39,9 +40,65 @@ public class Electrodomestico {
             precioFinal += 100;
         } else if (peso>=50 && peso<= 79) {
             precioFinal += 80;
+        } else if (peso>=20 && peso<= 49) {
+            precioFinal += 50;
+        } else if (peso>=0 && peso<= 19) {
+            precioFinal += 10;
         }
 
         return precioFinal;
+    }
+
+
+    protected Colores comprobarColor(String color) {
+        color = color.toLowerCase();
+        switch (color) {
+            case "black","negro"-> {
+                return Colores.NEGRO;
+            }
+            case "red","rojo"-> {
+                return Colores.ROJO;
+            }
+            case "blue","azul"-> {
+                return Colores.AZUL;
+            }
+            case "grey","gris"-> {
+                return Colores.GRIS;
+            }
+            default -> {
+                return Colores.BLANCO;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param consumo char de la entrada a comprobar
+     * @return Devuelve el valor dentro de un enumerado.
+     * Si el valor ingresado no se encuentra en el rango de la A a la F el valor por defecto es la F
+     */
+    protected Consumo comprobarConsumoEnergetico(char consumo){
+        consumo = Character.toLowerCase(consumo);
+        switch (consumo){
+            case 'a' -> {
+                return Consumo.A;
+            }
+            case 'b' -> {
+                return Consumo.B;
+            }
+            case 'c' -> {
+                return Consumo.C;
+            }
+            case 'd' -> {
+                return Consumo.D;
+            }
+            case 'e' -> {
+                return Consumo.E;
+            }
+            default -> {
+                return Consumo.F;
+            }
+        }
     }
 
 
@@ -66,5 +123,25 @@ public class Electrodomestico {
         return peso;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Electrodomestico that = (Electrodomestico) o;
+        return Double.compare(precioBase, that.precioBase) == 0 && Double.compare(peso, that.peso) == 0 && color == that.color && consumoEnergetico == that.consumoEnergetico;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(precioBase, color, consumoEnergetico, peso);
+    }
+
+    @Override
+    public String toString() {
+        return "Electrodomestico{" +
+                "precioBase=" + precioBase +
+                ", color=" + color +
+                ", consumoEnergetico=" + consumoEnergetico +
+                ", peso=" + peso +
+                '}';
+    }
 }
