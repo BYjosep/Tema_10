734ab69a10607e4054a707a83ab96f930a1ea214
Josep Vicent Mestre Llobell <59211313+BYjosep@users.noreply.github.com>
2025-05-08 02:48:36 +0200

    feat(Asiento): Añade funcionalidad y representación ANSI

--- src/main/java/com/BYjosep04/ejercicio7/zonas/Asiento.java
+++ src/main/java/com/BYjosep04/ejercicio7/zonas/Asiento.java
@@ -1,26 +1,61 @@
 package com.BYjosep04.ejercicio7.zonas;
 
 import com.BYjosep04.ejercicio7.interfaces.IAsiento;
+import lib.ANSI; // Importamos la librería ANSI
 
 public class Asiento implements IAsiento {
-    private boolean ocupado ;
+    private boolean ocupado;
+
+    /**
+     * Constructor para crear un asiento inicialmente libre.
+     */
     public Asiento() {
-        ocupado = false;
+        this.ocupado = false;
     }
 
+    /**
+     * Verifica si el asiento está ocupado.
+     * @return true si el asiento está ocupado, false si está libre.
+     */
     @Override
-    public boolean isOcupado(){
+    public boolean isOcupado() {
         return ocupado;
     }
 
+    /**
+     * Ocupa el asiento si está libre.
+     * @throws IllegalStateException Si el asiento ya está ocupado.
+     */
     @Override
     public void ocuparAsiento() {
+        if (ocupado) {
+            throw new IllegalStateException("El asiento ya está ocupado.");
+        }
         ocupado = true;
     }
 
+    /**
+     * Libera el asiento si está ocupado.
+     * @throws IllegalStateException Si el asiento ya está libre.
+     */
     @Override
     public void liberarAsiento() {
+        if (!ocupado) {
+            throw new IllegalStateException("El asiento ya está libre.");
+        }
         ocupado = false;
     }
 
+    /**
+     * Representación en cadena del asiento con colores ANSI.
+     * @return [X] en rojo si el asiento está ocupado, [ ] en verde si está libre.
+     */
+    @Override
+    public String toString() {
+        if (ocupado) {
+            return ANSI.format("[X]", true, ANSI.Color.RED, ANSI.Color.NONE); // Asiento ocupado en rojo
+        } else {
+            return ANSI.format("[ ]", true, ANSI.Color.GREEN, ANSI.Color.NONE); // Asiento libre en verde
+        }
+    }
 }
